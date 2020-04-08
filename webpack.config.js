8060c10dce753ddaae2575e28c0fee20702bc0bf
the-fukui <account@the-fukui.com>
2020-04-08 23:19:03 +0900

    babel loose mode

--- webpack.config.js
+++ webpack.config.js
@@ -4,56 +4,56 @@ const WriteFilePlugin = require('write-file-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-    // target:'node',
-    entry: `./src/index.js`,
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "index.js",
-        library: 'LaughAtIE',
-        libraryExport: 'default',
-        libraryTarget: 'umd',
-        globalObject: 'this',
-        umdNamedDefine: true
-    },
-    module: {
-        rules: [{
-                test: /\.js$/,
-                use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-env'
-                        ]
-                    },
-                }, ],
-                exclude: /node_modules/,
-            },
-            {
-                enforce: 'pre',
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                loader: 'eslint-loader',
-                options: {
-                    fix: true,
-                    configFile: '.eslintrc.js',
-                },
-            },
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({ template: "./src/index.html", inject: 'head' }),
-        new CopyWebpackPlugin(
-            [{
-                context: 'src/sound/',
-                from: '.',
-                to: path.resolve(__dirname, "dist/sound/")
-            }]),
-        new WriteFilePlugin()
-    ],
-    devServer: {
-        contentBase: "dist",
-        open: true,
-        host: '0.0.0.0',
-        disableHostCheck: true
-    }
+  // target:'node',
+  entry: `./src/index.js`,
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    library: 'LaughAtIE',
+    libraryExport: 'default',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [{
+        test: /\.js$/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { loose: true }]
+            ]
+          },
+        }, ],
+        exclude: /node_modules/,
+      },
+      {
+        enforce: 'pre',
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          fix: true,
+          configFile: '.eslintrc.js',
+        },
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./src/index.html", inject: 'head' }),
+    new CopyWebpackPlugin(
+      [{
+        context: 'src/sound/',
+        from: '.',
+        to: path.resolve(__dirname, "dist/sound/")
+      }]),
+    new WriteFilePlugin()
+  ],
+  devServer: {
+    contentBase: "dist",
+    open: true,
+    host: '0.0.0.0',
+    disableHostCheck: true
+  }
 };
